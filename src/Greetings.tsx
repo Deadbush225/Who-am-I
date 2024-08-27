5fa19ed3a0faab202ac949a9e72b67c64b07a67f
Eliazar Inso <eliazar225ninso@gmail.com>
2024-08-27 18:54:33 +0800

    added first page

---
+++ src/Greetings.tsx
@@ -0,0 +1,13 @@
+import "./Greetings.css";
+
+export default function Greetings() {
+	return (
+		<div className="text-left">
+			I'm <div className="bold cl-orange">Eliazar Inso</div>
+			<br></br>
+			<div className="subtitle">
+				an Aspiring Data Analyst & Software Developer
+			</div>
+		</div>
+	);
+}
---
+++ src/MenuBar.tsx
@@ -0,0 +1,19 @@
+import profileLogo from "./assets/jdenticon.svg";
+import "./MenuBar.css";
+
+export default function MenuBar() {
+	return (
+		<div className="hBoxLayout flex-Hcenter">
+			<img src={profileLogo} alt="Profile Icon" id="profileIcon" />
+
+			<div className="hBoxLayout flex-Vcenter">
+				<div className="menuItem" id="aboutMe">
+					About me
+				</div>
+				<div className="menuItem" id="contact">
+					Contact
+				</div>
+			</div>
+		</div>
+	);
+}
